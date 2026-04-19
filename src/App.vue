958fb38510f41ec2e87e775d4c9991da710386cf
lharyshu <Igor@lharyshu>
2026-04-19 23:48:36 +0300

    transfer

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <Header />
+
+  <main class="page">
+    <Notification />
+    <router-view />
+  </main>
+</template>
+
+<script>
+import Header from './components/Header.vue'
+import Notification from './components/Notification.vue';
+
+export default {
+  name: 'App',
+
+  components: {
+    Header,
+    Notification
+  }
+}
+</script>
